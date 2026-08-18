e7ed13ad76a5de876f34599495704950573f69f7
Sahitya Ghosh <sahityaghosh350@gmail.com>
2026-08-18 11:39:05 +0530

    Use environment variable for Razorpay secret
    Remove hardcoded Razorpay secret key.

--- src/utils/verifySignature.js
+++ src/utils/verifySignature.js
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
 export const verifyRazorpaySignature = (orderId, paymentId, signature) => {
-  const secret = process.env.RAZORPAY_KEY_SECRET || 'f7uhfL6OP9Xe7WMr9ITWLDVB';
+  const secret = process.env.RAZORPAY_KEY_SECRET;
   const body = orderId + '|' + paymentId;
   const expectedSignature = crypto
     .createHmac('sha256', secret)
